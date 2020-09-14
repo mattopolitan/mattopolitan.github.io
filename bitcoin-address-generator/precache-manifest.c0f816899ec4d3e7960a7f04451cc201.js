1db2058784ffe1f9f235f9c62d3f835f6187aa7f
mattopolitan <mattopolitan@gmail.com>
2020-09-14 23:08:22 +0800

    1. Added bitcoin-address-generator 2. Added dummy index page

---
+++ bitcoin-address-generator/precache-manifest.c0f816899ec4d3e7960a7f04451cc201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7781f320fc16b3f72f5a58104a84cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61332351bd650918a8cb",
+    "url": "./static/css/main.96ad9728.chunk.css"
+  },
+  {
+    "revision": "62dbf4acebf1d8ae505a",
+    "url": "./static/js/2.f0013a2f.chunk.js"
+  },
+  {
+    "revision": "c49e8ad363b7a28e74d10d1d5ae3d432",
+    "url": "./static/js/2.f0013a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61332351bd650918a8cb",
+    "url": "./static/js/main.38a81365.chunk.js"
+  },
+  {
+    "revision": "dc12d3ad8bead480fa1b",
+    "url": "./static/js/runtime-main.aca72535.js"
+  }
+]);
